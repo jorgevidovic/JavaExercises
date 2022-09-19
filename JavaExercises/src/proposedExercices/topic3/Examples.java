17679faa2fb5f7e8c9f9a9dffdec9eda199299b1
Jorge-I-Vidovic <jorgeisaacvidovich@gmail.com>
2022-09-19 19:49:14 +0200

    Updating project

---
+++ JavaExercises/src/proposedExercices/topic3/Examples.java
@@ -0,0 +1,9 @@
+package proposedExercices.topic3;
+import java.util.*;
+public class Examples {
+	public static void main(String[] args) {
+	//Archive to solve some examples
+		String cadena = "Que la fuerza te acompañe";
+		System.out.println(cadena.substring(1,2));
+	}
+}
