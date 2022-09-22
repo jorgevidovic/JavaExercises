95ebf9cea8ddaf438da91127f43ef934e016b8eb
Jorge-I-Vidovic <jorgeisaacvidovich@gmail.com>
2022-09-22 13:25:54 +0200

    Updating project

--- JavaExercises/src/proposedExercices/topic3/MaxAndMinCalculator.java
+++ JavaExercises/src/proposedExercices/topic3/MaxAndMinCalculator.java
@@ -1,6 +1,22 @@
 package proposedExercices.topic3;
+import java.text.DecimalFormat;
+import javax.swing.JOptionPane;
+
 public class MaxAndMinCalculator {
 	public static void main(String[] args) {
+		DecimalFormat f = new DecimalFormat("#.##");
+		JOptionPane.showMessageDialog(null, "Programa para calcular el máximo y mínimo de 2 números introducidos por teclado.");
+		
+		float a;
+		float b;
+		
+		a = Float.parseFloat(JOptionPane.showInputDialog("Introduce el 1er valor:"));
+		b = Float.parseFloat(JOptionPane.showInputDialog("Introduce el 2do valor:"));
+		
+		float m = Math.min(a, b);
+		float M = Math.max(a, b);
+		
+		JOptionPane.showMessageDialog(null, "El valor mínimo es: " +m +"\nEl valor máximo es: "+M);
 		
 	}
 }
