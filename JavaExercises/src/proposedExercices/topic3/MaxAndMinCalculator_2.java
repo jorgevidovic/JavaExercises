e78fe4bc88cceb55fcc6929ce436613496011958
Jorge-I-Vidovic <jorgeisaacvidovich@gmail.com>
2022-09-23 12:05:30 +0200

    Updating project

--- JavaExercises/src/proposedExercices/topic3/MaxAndMinCalculator_2.java
+++ JavaExercises/src/proposedExercices/topic3/MaxAndMinCalculator_2.java
@@ -1,23 +1,25 @@
 package proposedExercices.topic3;
 import java.text.DecimalFormat;
+import java.util.Arrays;
 import javax.swing.JOptionPane;
 
 public class MaxAndMinCalculator_2 {
-
+	
 	public static void main(String[] args) {
 		DecimalFormat f = new DecimalFormat("#.##");
-		JOptionPane.showMessageDialog(null, "Programa para calcular el máximo y mínimo de 2 números introducidos por teclado.");
+		JOptionPane.showMessageDialog(null, "Programa para calcular el máximo y mínimo de 3 números introducidos por teclado.");
 		
-		float a;
-		float b;
+		float d[] = new float[3];
 		
-		a = Float.parseFloat(JOptionPane.showInputDialog("Introduce el 1er valor:"));
-		b = Float.parseFloat(JOptionPane.showInputDialog("Introduce el 2do valor:"));
+		d[0] = Float.parseFloat(JOptionPane.showInputDialog("Introduce el 1er valor:"));
+		d[1] = Float.parseFloat(JOptionPane.showInputDialog("Introduce el 2do valor:"));
+		d[2] = Float.parseFloat(JOptionPane.showInputDialog("Introduce el 3er valor:"));
 		
-		float m = Math.min(a, b);
-		float M = Math.max(a, b);
+		Arrays.sort(d);
+		float m = d[0];
+		float M = d[2];
 		
-		JOptionPane.showMessageDialog(null, "El valor mínimo es: " +m +"\nEl valor máximo es: "+M);
+		JOptionPane.showMessageDialog(null, "El valor mínimo es: " +m+"\nEl valor máximo es: "+M);
 
 	}
 
