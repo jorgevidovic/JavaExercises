87f2667ba7610d1b802991fa3146df4615ccfeed
Jorge-I-Vidovic <jorgeisaacvidovich@gmail.com>
2022-09-24 18:59:02 +0200

    Updating project

---
+++ JavaExercises/src/proposedExercices/topic3/ArrayCopier.java
@@ -0,0 +1,12 @@
+package proposedExercices.topic3;
+
+import javax.swing.JOptionPane;
+
+public class ArrayCopier {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Programa para ordenar matrices de 5 valores.");
+		
+		
+	}
+
+}
