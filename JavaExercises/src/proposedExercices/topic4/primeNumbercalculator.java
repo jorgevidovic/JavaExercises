9c68c721ddff7e2b17f69f7d80f3348a30da6f7e
Jorge-I-Vidovic <jorgeisaacvidovich@gmail.com>
2022-10-03 20:07:32 +0200

    Updating project

--- JavaExercises/src/proposedExercices/topic4/primeNumbercalculator.java
+++ JavaExercises/src/proposedExercices/topic4/primeNumbercalculator.java
@@ -1,7 +1,17 @@
 package proposedExercices.topic4;
 
+import javax.swing.JOptionPane;
+
 public class primeNumbercalculator {
 	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Programa para calcular si un número es primo.");
+		float x = Float.parseFloat(JOptionPane.showInputDialog("Introduce un número: "));
 		
+		if (x%2==0) {
+			JOptionPane.showMessageDialog(null, "El número introducido es: "+x
+											 +"\nEl número introducido es par.");
+		} else
+		JOptionPane.showMessageDialog(null, "El número introducido es: "+x
+				                         +"\nEl número introducdio es impar.");
 	}
 }
