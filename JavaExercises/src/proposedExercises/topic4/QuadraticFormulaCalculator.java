72e3f60f77ec66734be15a6da810e18c70710598
Jorge-I-Vidovic <jorgeisaacvidovich@gmail.com>
2022-10-04 19:12:21 +0200

    Updating project

--- JavaExercises/src/proposedExercises/topic4/QuadraticFormulaCalculator.java
+++ JavaExercises/src/proposedExercises/topic4/QuadraticFormulaCalculator.java
@@ -10,13 +10,13 @@ public class QuadraticFormulaCalculator {
 			float b = Float.parseFloat(JOptionPane.showInputDialog("Introduce la variable B: "));
 			float c = Float.parseFloat(JOptionPane.showInputDialog("Introduce la variable C: "));
 			
-			float x;
-			float x1;
-			float x2;
+			float x = 0;
+			float x1 = 0;
+			float x2 = 0;
 			
 			float isReal =(float)Math.sqrt(Math.pow(b, 2)-4*a*c);
 			
-			if(a==0){
+			if(Float.compare(a,0) == 0){
 				x = -(c/b);
 				JOptionPane.showMessageDialog(null, "Valores introducidos: "
 					    +"\na: "+a
@@ -27,18 +27,19 @@ public class QuadraticFormulaCalculator {
 						+"\nx2: "+x);
 			}
 			
-			else if(b==0){
-				x = (float)Math.sqrt(-(c/b));
+			else if(Float.compare(b,0) == 0){
+				x1 = (float)Math.sqrt(-(c/b));
+				x2 = -(x1); 
 				JOptionPane.showMessageDialog(null, "Valores introducidos: "
                         +"\na: "+a
                         +"\nb: "+b
                         +"\nc: "+c
                         +"\n----------"
-                        +"\nx1: "+x
-                        +"\nx2: "+x);
+                        +"\nx1: "+x1
+                        +"\nx2: "+x2);
 			}
 			
-			else if(c==0){
+			else if(Float.compare(c,0) == 0){
 				x1 = 0;
 				x2 = -(b/a);
 				
