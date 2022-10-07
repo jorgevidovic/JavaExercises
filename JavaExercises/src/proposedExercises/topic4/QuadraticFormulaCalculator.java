37c64e36fc4357551fb2a8f7ac8ff244aa099958
Jorge-I-Vidovic <jorgeisaacvidovich@gmail.com>
2022-10-07 17:43:20 +0200

    Error solved in CuadraticFormulaCalculator.java

--- JavaExercises/src/proposedExercises/topic4/QuadraticFormulaCalculator.java
+++ JavaExercises/src/proposedExercises/topic4/QuadraticFormulaCalculator.java
@@ -5,58 +5,69 @@ public class QuadraticFormulaCalculator {
 	public static void main(String[] args) {
 		try {
 			JOptionPane.showMessageDialog(null, "Programa para calcular una raíz cuadrada.");
+		
+			int a = Integer.parseInt(JOptionPane.showInputDialog("Introduce la variable A: "));
+			int b = Integer.parseInt(JOptionPane.showInputDialog("Introduce la variable B: "));
+			int c = Integer.parseInt(JOptionPane.showInputDialog("Introduce la variable C: "));
 			
-			float a = Float.parseFloat(JOptionPane.showInputDialog("Introduce la variable A: "));
-			float b = Float.parseFloat(JOptionPane.showInputDialog("Introduce la variable B: "));
-			float c = Float.parseFloat(JOptionPane.showInputDialog("Introduce la variable C: "));
+			double x1 = 0;
+			double x2 = 0;
 			
-			float x = 0;
-			float i = 0;
-			float x1 = 0;
-			float x2 = 0;
+			boolean solution = false;
 			
-			float isReal =(float)Math.sqrt((float)Math.pow(b, 2)-4*a*c);
-			
-			if(Float.compare(a,i) ==0){
-				x = -(c/b);
-				JOptionPane.showMessageDialog(null, "Valores introducidos: "
-					    +"\na: "+a
-						+"\nb: "+b
-						+"\nc: "+c
-						+"\n----------"
-						+"\nx1: "+x
-						+"\nx2: "+x);
-			}
-			
-			else if(Float.compare(b,i) == 0){
-				x1 = (float)Math.sqrt(-(c/b));
-				x2 = -(x1); 
-				JOptionPane.showMessageDialog(null, "Valores introducidos: "
-                        +"\na: "+a
-                        +"\nb: "+b
-                        +"\nc: "+c
-                        +"\n----------"
-                        +"\nx1: "+x1
-                        +"\nx2: "+x2);
-			}
-			
-			else if(Float.compare(c,i) == 0){
-				x1 = 0;
-				x2 = -(b/a);
+			//------------- Inicio del caso A -------------
+			if(a==0){
+				//bx + c = 0; --> x1 = x2 = -c/b
 				
-				JOptionPane.showMessageDialog(null, "Valores introducidos: "
-                        +"\na: "+a
-                        +"\nb: "+b
-                        +"\nc: "+c
-                        +"\n----------"
-                        +"\nx1: "+x1
-                        +"\nx2: "+x2);
+				if(b==0) {
+					JOptionPane.showMessageDialog(null, "Error, no existe solución.");
+				} else{
+					solution = true;
+					x1 = x2 = -c/b;
+				}
 			}
+			
+			//------------- Inicio del caso B -------------
+			else if(b==0){	
+				//ax2 + c = 0 --> x1 = Raíz(-c/a) , x2 = -Raíz(-c/a)
+			
+				double isReal = -c/a;
+				if(isReal>0) {
+					solution = true;
+					x1 = Math.sqrt(isReal);
+					x2 = -x1; 
+				} else {
+					JOptionPane.showMessageDialog(null, "Error, no existe solución.");
+				}
+			} 
+			
+			//------------- Inicio del caso C -------------
+			
+			else if(c==0){
+				//ax2 + bx = 0 --> x(ax+b) = 0 --> x1 = 0 , x2 = -b/a;
+				solution = true;
+				x1 = 0;
+				x2 = -b/a;	
+			}
+			
+			//------------- Inicio del caso standard -------------
 			
 			else {
-				x1 = (-b+isReal)/2*a;	
-				x2 = (-b-isReal)/2*a;
 				
+				//ax2 + bx + c = 0 -->x1 = -b + Raiz(b2 -4ac)/2a ; x2 = -b - Raiz(b2-4ac)/2a
+				double isReal = Math.sqrt((b*b)-(4*a*c));
+				
+				if (isReal>0) {
+					solution = true;
+					x1 = (-b+isReal)/2*a;	
+					x2 = (-b-isReal)/2*a;
+					
+				} else {
+					JOptionPane.showMessageDialog(null, "Error, no existe solución.");
+				}
+			}
+			
+			if (solution == true){
 				JOptionPane.showMessageDialog(null, "Valores introducidos: "
                         +"\na: "+a
                         +"\nb: "+b
@@ -64,11 +75,10 @@ public class QuadraticFormulaCalculator {
                         +"\n----------"
                         +"\nx1: "+x1
                         +"\nx2: "+x2);
-				
-				//Existe un error --> Apunta directamente a este segmento de código
 			}
+			
 		} catch(Exception e) {
-			JOptionPane.showMessageDialog(null, "Algo ha ido mal, intentalo de nuevo.");
+			JOptionPane.showMessageDialog(null, "Error, has introducido un caracter incorrecto.");
 		}
 	}
 }
