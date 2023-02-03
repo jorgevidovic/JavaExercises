a87ca05d1fac16e91e5b340e895c1374c6066289
Jorge I. Vidovic <jorgeisaacvidovich@gmail.com>
2023-02-03 19:56:14 +0100

    Create Test1.java

---
+++ JavaExercises/src/Test1.java
@@ -0,0 +1,11 @@
+
+public class Test1 {
+	public static void main(String[] args) {
+		String s = "java";
+		s.toUpperCase();
+		s.trim();
+		s.substring(1, 3);
+		s += " nueve";
+		System.out.println(s);
+	}
+}
