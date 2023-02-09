ea62a7fb33cad9827a241c7f9eadd5878c81c636
Jorge I. Vidovic <jorgeisaacvidovich@gmail.com>
2023-02-09 20:46:16 +0100

    Jeje

--- Theme 7 - Enheritance/src/exercise1/Directivo.java
+++ Theme 7 - Enheritance/src/exercise1/Directivo.java
@@ -3,10 +3,25 @@ package exercise1;
 import java.time.LocalDate;
 
 public class Directivo extends Empleado{
+	String numTelf;
 
-	public Directivo(String nombre, String dni, LocalDate fechaNacimiento, LocalDate fechaContratacion) {
+	public Directivo(String nombre, String dni, LocalDate fechaNacimiento, LocalDate fechaContratacion, String numTelf) {
 		super(nombre, dni, fechaNacimiento, fechaContratacion);
-		
+		this.numTelf = numTelf;
+	}
+
+	public String getNumTelf() {
+		return numTelf;
+	}
+
+	public void setNumTelf(String numTelf) {
+		this.numTelf = numTelf;
+	}
+	
+	@Override
+	public void setSalario(float salario) {
+		int antiguedad = calcularAntiguedad();
+		this.salario = salarioBase + (antiguedad / 3) * salarioBase * 0.2f;
 	}
 
 }
--- Theme 7 - Enheritance/src/exercise1/Principal.java
+++ Theme 7 - Enheritance/src/exercise1/Principal.java
@@ -6,34 +6,36 @@ import javax.swing.JOptionPane;
 
 public class Principal {
 	public static void main(String[] args) {
-		char o = 0;
+		DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		String nombre, dni, fechaN, fechaC;
+		LocalDate fechaNacimiento, fechaContratacion;
 		Empleado[] plantilla = new Empleado[10];
+		char o = 0;
 
 		do {
-			o = JOptionPane.showInputDialog(null, 
-							"- Gestor de empleados -" 
-							+ "\na) Guardar un operario." 
-							+ "\nb) Guardar un administrativo."
-							+ "\nc) Guardar un directivo." 
-							+ "\nb) Actualizar salario de los empleados." 
-							+ "\nx) Salir.")
+			o = JOptionPane.showInputDialog(null,
+					"- Gestor de empleados -" + "\na) Guardar un operario." + "\nb) Guardar un administrativo."
+							+ "\nc) Guardar un directivo." + "\nd) Actualizar salario de los empleados."
+							+ "\ne) Mostrar todos los empleados." + "\nx) Salir.")
 					.toLowerCase().charAt(0);
 			switch (o) {
-			case 'a':
-				DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-				String nombre = JOptionPane.showInputDialog("Introduce el nombre del operario:");
-				String dni = JOptionPane.showInputDialog("Introduce el DNI:");
-				String fechaN = JOptionPane.showInputDialog("Introduce la fecha de nacimiento del operario ");
-				String fechaC = JOptionPane.showInputDialog("Introduce la fecha de contratacion: ");
 
-				LocalDate fechaNacimiento = LocalDate.parse(fechaN, formato);
-				LocalDate fechaContratacion = LocalDate.parse(fechaC, formato);
-				
-				Empleado empleado = new Empleado(nombre, dni, fechaNacimiento, fechaContratacion);
-				Empleado.guardarEmpleado(empleado, plantilla);
-				
+			case 'a':
+				nombre = JOptionPane.showInputDialog("Introduce el nombre del operario:");
+				dni = JOptionPane.showInputDialog("Introduce el DNI:");
+				fechaN = JOptionPane.showInputDialog("Introduce la fecha de nacimiento del operario ");
+				fechaC = JOptionPane.showInputDialog("Introduce la fecha de contratación: ");
+				String seguroAccidentes = JOptionPane.showInputDialog("Introduce el número de poliza de accidentes: ");
+				String responsabilidadCivil = JOptionPane.showInputDialog("Introduce el número de poliza de accidentes: ");
+
+				fechaNacimiento = LocalDate.parse(fechaN, formato);
+				fechaContratacion = LocalDate.parse(fechaC, formato);
+
+				Operario operario = new Operario(nombre, dni, fechaNacimiento, fechaContratacion, seguroAccidentes,responsabilidadCivil);
+				Empleado.guardarEmpleado(operario, plantilla);
+
 				for (Empleado empleado2 : plantilla) {
-					if(empleado2!=null)
+					if (empleado2 != null)
 						System.out.println(empleado2);
 				}
 			}
