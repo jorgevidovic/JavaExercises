a97a2a17c31b3497ab7b3e5132aaff198e0b9bce
Jorge I. Vidovic <jorgeisaacvidovich@gmail.com>
2023-03-08 19:45:24 +0100

    xd

--- Unknown/src/examen/OVNI.java
+++
@@ -1,9 +0,0 @@
-package examen;
-
-public class OVNI extends ObjetoEspacial {
-
-	public OVNI(float masa, float velocidad, String nombre) {
-		super(masa, velocidad, nombre);
-		setNombre("Desconocido");
-	}
-}
