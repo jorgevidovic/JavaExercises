abe72c5c7f9d2066c770540bbe45e3c7f5d3f92b
Jorge I. Vidovic <jorgeisaacvidovich@gmail.com>
2023-06-01 12:30:25 +0200

    Create ModificaBDD.java

---
+++ BDD_JDBC/src/BDD/ModificaBDD.java
@@ -0,0 +1,26 @@
+package BDD;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class ModificaBDD {
+	public static void main(String[] args) {
+		try {
+			// 1.Crear Conexión
+			Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+
+			// 2. Crear objeto Statement
+			Statement statement = conexion.createStatement();
+			String instruccionSql = "INSERT INTO test.persona (nombre, apellido, email, telefono) VALUES ('Dayanet', 'Chourio', 'correo@correo.com', '963852751')";
+			
+			//3. Ejecutar instrucción
+			statement.executeUpdate(instruccionSql);
+			System.out.println("Datos insertados correctamente!");
+
+		} catch (Exception e) {
+			System.out.println("No conecta!");
+			e.printStackTrace();
+		}
+	}
+}
